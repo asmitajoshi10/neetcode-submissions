e958b266c6fada10291f6964c4bfb5c418c85d65
Asmita Joshi <241230016@nitdelhi.ac.in>
2026-07-05 00:04:12 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        int complement = 0;
+        for( int i=0; i<nums.size(); i++){
+            complement = target - nums[i];
+            if(mp.count(complement)){
+                return{mp[complement],i};
+            }
+            mp[nums[i]]=i;
+        } return {};
+    }
+};
